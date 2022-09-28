229b0c50861983c865afa27b4afb3fe15ff9ace1
sabbir <sabbir@gmail.com>
2022-09-28 21:47:06 +0600

    Home component added

---
+++ src/components/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div className='home'>
+            <div className="exercise">
+
+            </div>
+            <div className="cart">
+
+            </div>
+        </div>
+    );
+};
+
+export default Home;
